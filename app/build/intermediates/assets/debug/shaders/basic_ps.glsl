344855de8b43352d15438a227114ba6521c6e300
pourquoi <mathias.dusautoy@gmail.com>
2015-06-11 15:10:04 +0200

    remove build

--- app/build/intermediates/assets/debug/shaders/basic_ps.glsl
+++
@@ -1,6 +0,0 @@
-precision mediump float;
-varying vec4 vColor;
-varying vec3 vPos;
-void main() {
-	gl_FragColor = vColor;
-}
--- app/build/intermediates/assets/debug/shaders/sprite_ps.glsl
+++
@@ -1,8 +0,0 @@
-precision mediump float;
-varying vec2 vTextureCoord;
-varying vec4 vColor;
-varying vec3 vPos;
-uniform sampler2D sTexture0;
-void main() {
-	gl_FragColor = texture2D(sTexture0, vTextureCoord);
-}
